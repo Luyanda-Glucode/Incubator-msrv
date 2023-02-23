4f60c7a1c451e8323c1e81ee9f02db132f6d460e
Luyanda-Glucode <117176161+Luyanda-Glucode@users.noreply.github.com>
2023-02-23 09:57:15 +0200

    swagger codegen generated class and model

---
+++ target/generated-sources/swagger/src/gen/java/main/io/swagger/client/api/WeatherApi.java
@@ -0,0 +1,83 @@
+package io.swagger.client.api;
+
+import io.swagger.client.ApiClient;
+
+import io.swagger.client.model.WeatherModel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2023-02-23T09:56:17.912+02:00")
+@Component("io.swagger.client.api.WeatherApi")
+public class WeatherApi {
+    private ApiClient apiClient;
+
+    public WeatherApi() {
+        this(new ApiClient());
+    }
+
+    @Autowired
+    public WeatherApi(ApiClient apiClient) {
+        this.apiClient = apiClient;
+    }
+
+    public ApiClient getApiClient() {
+        return apiClient;
+    }
+
+    public void setApiClient(ApiClient apiClient) {
+        this.apiClient = apiClient;
+    }
+
+    /**
+     * Get weather summery
+     * 
+     * <p><b>405</b> - Invalid input
+     * @param body Current weather
+     * @throws RestClientException if an error occurs while attempting to invoke the API
+     */
+    public void getWeather(WeatherModel body) throws RestClientException {
+        Object postBody = body;
+        
+        // verify the required parameter 'body' is set
+        if (body == null) {
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Missing the required parameter 'body' when calling getWeather");
+        }
+        
+        String path = UriComponentsBuilder.fromPath("/weather").build().toUriString();
+        
+        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<String, String>();
+        final HttpHeaders headerParams = new HttpHeaders();
+        final MultiValueMap<String, Object> formParams = new LinkedMultiValueMap<String, Object>();
+
+        final String[] accepts = { 
+            "application/json", "application/xml"
+        };
+        final List<MediaType> accept = apiClient.selectHeaderAccept(accepts);
+        final String[] contentTypes = { 
+            "application/json", "application/xml"
+        };
+        final MediaType contentType = apiClient.selectHeaderContentType(contentTypes);
+
+        String[] authNames = new String[] { "weather_auth" };
+
+        ParameterizedTypeReference<Void> returnType = new ParameterizedTypeReference<Void>() {};
+        apiClient.invokeAPI(path, HttpMethod.GET, queryParams, postBody, headerParams, formParams, accept, contentType, authNames, returnType);
+    }
+}
