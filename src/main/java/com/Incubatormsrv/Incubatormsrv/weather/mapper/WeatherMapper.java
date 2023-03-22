6bff6e2d167ce33bb4f31cf2513b927834ac668b
Luyanda-Glucode <117176161+Luyanda-Glucode@users.noreply.github.com>
2023-03-22 15:46:52 +0200

    Removed commented code

--- src/main/java/com/Incubatormsrv/Incubatormsrv/weather/mapper/WeatherMapper.java
+++ src/main/java/com/Incubatormsrv/Incubatormsrv/weather/mapper/WeatherMapper.java
@@ -15,27 +15,4 @@ public interface WeatherMapper {
 
         WeatherResponse weatherApiMapper(WeatherModel weather);
     }
-
-
-//    public default weatherModel weatherMapper(WeatherApi weatherApi) {
-//        weatherModel weather = new weatherModel();
-//
-//        return weather;
-//    }
-//
-//    private weatherModel weatherModelMappingWithApi(WeatherResponse weatherResponse) {
-//        weatherModel weather = new weatherModel();
-//
-//        weather.setName(weatherResponse.getLocation().getName());
-//        weather.setRegion(weatherResponse.getLocation().getRegion());
-//        weather.setCountry(weatherResponse.getLocation().getCountry());
-//        weather.setLocaltime(weatherResponse.getLocation().getLocaltime());
-//        weather.setTemp_c(weatherResponse.getCurrent().getTempC());
-//        weather.setTemp_f(weatherResponse.getCurrent().getTempF());
-//        weather.setLast_updated(weatherResponse.getCurrent().getLastUpdated());
-//        weather.setText(weatherResponse.getCurrent().getCondition().getText());
-//        weather.setIcon(weatherResponse.getCurrent().getCondition().getIcon());
-//
-//        return weather;
-//    }
 }
