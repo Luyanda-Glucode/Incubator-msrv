6d75bb56662c7cc1cf4a1f291cad4c0709313d32
Luyanda-Glucode <117176161+Luyanda-Glucode@users.noreply.github.com>
2023-04-05 09:58:55 +0200

    added the response status exception

--- src/main/java/com/Incubatormsrv/Incubatormsrv/weather/WeatherApiException.java
+++ src/main/java/com/Incubatormsrv/Incubatormsrv/weather/WeatherApiException.java
@@ -1,5 +1,9 @@
 package com.Incubatormsrv.Incubatormsrv.weather;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class WeatherApiException extends RuntimeException {
     public WeatherApiException(String message) {
         super(message);
